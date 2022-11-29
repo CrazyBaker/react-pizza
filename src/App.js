1c64f8f00a453d5d23782fb873e8e14b8afc03b4
CrazyBacker <zigunovchrome@gmail.com>
2022-11-29 14:53:25 +0300

    add mockapi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './scss/app.scss';
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/ PizzaBlock';
-import pizzas from './assets/pizzas.json';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch('https://6385c9b7875ca3273d457334.mockapi.io/pizzas')
+      .then((res) => {
+        return res.json();
+      })
+      .then((pizzas) => {
+        setPizzas(pizzas);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
